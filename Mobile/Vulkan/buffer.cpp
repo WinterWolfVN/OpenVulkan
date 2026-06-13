1641926f6a3735569324998d15050639e4d23a24
The Earth 🌏 <winterwolfvn123@gmail.com>
2026-06-13 08:31:20 +0700

    Update buffer.cpp

--- Mobile/Vulkan/buffer.cpp
+++ Mobile/Vulkan/buffer.cpp
@@ -1,11 +1,20 @@
 #include <GLES3/gl31.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef void* VkDevice;
 typedef uint64_t VkBuffer;
+typedef uint64_t VkDeviceMemory;
+typedef uint64_t VkDeviceSize;
 typedef uint32_t VkResult;
 
 #define VK_SUCCESS 0
+
+struct FakeMemory {
+    void* cpuData;
+    VkDeviceSize size;
+    GLuint boundBufferID;
+};
 
 extern "C" {
 
@@ -21,6 +30,46 @@ void vkDestroyBuffer(VkDevice device, VkBuffer buffer, const void* pAllocator) {
     glDeleteBuffers(1, &bufferID);
 }
 
+VkResult vkAllocateMemory(VkDevice device, const void* pAllocateInfo, const void* pAllocator, VkDeviceMemory* pMemory) {
+    VkDeviceSize size = *(VkDeviceSize*)((char*)pAllocateInfo + 16); 
+    
+    FakeMemory* mem = (FakeMemory*)malloc(sizeof(FakeMemory));
+    mem->cpuData = malloc(size);
+    mem->size = size;
+    mem->boundBufferID = 0;
+
+    *pMemory = (VkDeviceMemory)mem;
+    return VK_SUCCESS;
+}
+
+void vkFreeMemory(VkDevice device, VkDeviceMemory memory, const void* pAllocator) {
+    if (memory) {
+        FakeMemory* mem = (FakeMemory*)memory;
+        free(mem->cpuData);
+        free(mem);
+    }
+}
+
+VkResult vkBindBufferMemory(VkDevice device, VkBuffer buffer, VkDeviceMemory memory, VkDeviceSize memoryOffset) {
+    FakeMemory* mem = (FakeMemory*)memory;
+    mem->boundBufferID = (GLuint)buffer;
+    return VK_SUCCESS;
+}
+
+VkResult vkMapMemory(VkDevice device, VkDeviceMemory memory, VkDeviceSize offset, VkDeviceSize size, uint32_t flags, void** ppData) {
+    FakeMemory* mem = (FakeMemory*)memory;
+    *ppData = (char*)mem->cpuData + offset;
+    return VK_SUCCESS;
+}
+
+void vkUnmapMemory(VkDevice device, VkDeviceMemory memory) {
+    FakeMemory* mem = (FakeMemory*)memory;
+    if (mem->boundBufferID != 0) {
+        glBindBuffer(GL_ARRAY_BUFFER, mem->boundBufferID);
+        glBufferData(GL_ARRAY_BUFFER, mem->size, mem->cpuData, GL_STATIC_DRAW);
+    }
+}
+
 void vkCmdBindVertexBuffers(void* commandBuffer, uint32_t firstBinding, uint32_t bindingCount, const VkBuffer* pBuffers, const uint64_t* pOffsets) {
     if (bindingCount > 0) {
         glBindBuffer(GL_ARRAY_BUFFER, (GLuint)pBuffers[0]);
@@ -34,5 +83,6 @@ void vkCmdBindIndexBuffer(void* commandBuffer, VkBuffer buffer, uint64_t offset,
 void CreateAndBindVAO(GLuint* vaoID) {
     glGenVertexArrays(1, vaoID);
     glBindVertexArray(*vaoID);
-   }
+}
+
 }
