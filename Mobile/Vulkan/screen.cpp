2b12cce31d87124f221b99115101bded876a3e63
The Earth 🌏 <winterwolfvn123@gmail.com>
2026-06-13 09:24:19 +0700

    Update screen.cpp

--- Mobile/Vulkan/screen.cpp
+++ Mobile/Vulkan/screen.cpp
@@ -1,47 +1,32 @@
-#include <EGL/egl.h>
 #include <GLES3/gl31.h>
+#include <stdint.h>
 
-typedef void* VkCommandBuffer;
-typedef void* VkQueue;
+typedef void* VkDevice;
+typedef uint64_t VkSwapchainKHR;
 typedef uint32_t VkResult;
 
 #define VK_SUCCESS 0
 
-extern EGLDisplay global_display;
-
-struct ClearColor { float r, g, b, a; };
-struct RenderPassBegin {
-    void* renderPass;
-    void* framebuffer;
-    uint32_t renderArea[4];
-    uint32_t clearValueCount;
-    ClearColor* pClearValues;
-};
-
 extern "C" {
 
-void vkCmdBeginRenderPass(VkCommandBuffer commandBuffer, const RenderPassBegin* pBegin, uint32_t contents) {
-    if (pBegin && pBegin->clearValueCount > 0) {
-        glClearColor(pBegin->pClearValues[0].r, pBegin->pClearValues[0].g, pBegin->pClearValues[0].b, pBegin->pClearValues[0].a);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
-    }
-}
-
-void vkCmdEndRenderPass(VkCommandBuffer commandBuffer) {}
-
-void vkCmdDraw(VkCommandBuffer commandBuffer, uint32_t vertexCount, uint32_t instanceCount, uint32_t firstVertex, uint32_t firstInstance) {
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
-    glDrawArrays(GL_TRIANGLES, firstVertex, vertexCount);
-}
-
-VkResult vkQueuePresentKHR(VkQueue queue, const void* pPresentInfo) {
-    eglSwapBuffers(global_display, eglGetCurrentSurface(EGL_DRAW));
+VkResult vkCreateSwapchainKHR(VkDevice device, const void* pCreateInfo, const void* pAllocator, VkSwapchainKHR* pSwapchain) {
+    if (pSwapchain) *pSwapchain = (VkSwapchainKHR)1;
     return VK_SUCCESS;
 }
 
-VkResult vkAcquireNextImageKHR(void* device, void* swapchain, uint64_t timeout, void* sem, void* fence, uint32_t* pIndex) {
-    if (pIndex) *pIndex = 0;
+void vkDestroySwapchainKHR(VkDevice device, VkSwapchainKHR swapchain, const void* pAllocator) {}
+
+VkResult vkGetSwapchainImagesKHR(VkDevice device, VkSwapchainKHR swapchain, uint32_t* pCount, void* pImages) {
+    if (pCount) *pCount = 1;
+    return VK_SUCCESS;
+}
+
+VkResult vkAcquireNextImageKHR(VkDevice device, VkSwapchainKHR swapchain, uint64_t timeout, void* semaphore, void* fence, uint32_t* pImageIndex) {
+    if (pImageIndex) *pImageIndex = 0;
+    return VK_SUCCESS;
+}
+
+VkResult vkQueuePresentKHR(void* queue, const void* pPresentInfo) {    
     return VK_SUCCESS;
 }
 
