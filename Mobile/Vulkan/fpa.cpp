a61add782e580ee4d65c40ac89a39c3568016aab
The Earth 🌏 <winterwolfvn123@gmail.com>
2026-06-15 20:51:56 +0700

    Update fpa.cpp

--- Mobile/Vulkan/fpa.cpp
+++ Mobile/Vulkan/fpa.cpp
@@ -1,80 +1,87 @@
 #include <string.h>
 
-struct FPAEntry { const char* name; PFN_vkVoidFunction func; };
+struct FPAEntry {
+    const char* name;
+    PFN_vkVoidFunction func;
+};
 
 static const FPAEntry dispatch_table[] = {
+
     // [buffer.cpp]
-    {"vkCreateBuffer", (PFN_vkVoidFunction)vkCreateBuffer}, 
-    {"vkDestroyBuffer", (PFN_vkDestroyBuffer)vkDestroyBuffer},
-    {"vkAllocateMemory", (PFN_vkAllocateMemory)vkAllocateMemory}, 
-    {"vkFreeMemory", (PFN_vkFreeMemory)vkFreeMemory},
-    {"vkBindBufferMemory", (PFN_vkBindBufferMemory)vkBindBufferMemory}, 
-    {"vkMapMemory", (PFN_vkMapMemory)vkMapMemory},
-    {"vkUnmapMemory", (PFN_vkUnmapMemory)vkUnmapMemory}, 
+    {"vkCreateBuffer", (PFN_vkVoidFunction)vkCreateBuffer},
+    {"vkDestroyBuffer", (PFN_vkVoidFunction)vkDestroyBuffer},
+    {"vkAllocateMemory", (PFN_vkVoidFunction)vkAllocateMemory},
+    {"vkFreeMemory", (PFN_vkVoidFunction)vkFreeMemory},
+    {"vkBindBufferMemory", (PFN_vkVoidFunction)vkBindBufferMemory},
+    {"vkMapMemory", (PFN_vkVoidFunction)vkMapMemory},
+    {"vkUnmapMemory", (PFN_vkVoidFunction)vkUnmapMemory},
     {"vkCreatePipelineLayout", (PFN_vkVoidFunction)vkCreatePipelineLayout},
-    {"vkDestroyPipelineLayout", (PFN_vkDestroyPipelineLayout)vkDestroyPipelineLayout}, 
+    {"vkDestroyPipelineLayout", (PFN_vkVoidFunction)vkDestroyPipelineLayout},
     {"vkCreateGraphicsPipelines", (PFN_vkVoidFunction)vkCreateGraphicsPipelines},
-    {"vkDestroyPipeline", (PFN_vkDestroyPipeline)vkDestroyPipeline},
-    
+    {"vkDestroyPipeline", (PFN_vkVoidFunction)vkDestroyPipeline},
+
     // [command.cpp]
-    {"vkCreateCommandPool", (PFN_vkVoidFunction)(PFN_vkCreateCommandPool)vkCreateCommandPool},
-    {"vkDestroyCommandPool", (PFN_vkVoidFunction)(PFN_vkDestroyCommandPool)vkDestroyCommandPool},
-    {"vkAllocateCommandBuffers", (PFN_vkVoidFunction)(PFN_vkAllocateCommandBuffers)vkAllocateCommandBuffers},
-    {"vkFreeCommandBuffers", (PFN_vkVoidFunction)(PFN_vkFreeCommandBuffers)vkFreeCommandBuffers},
-    {"vkBeginCommandBuffer", (PFN_vkVoidFunction)(PFN_vkBeginCommandBuffer)vkBeginCommandBuffer},
-    {"vkEndCommandBuffer", (PFN_vkVoidFunction)(PFN_vkEndCommandBuffer)vkEndCommandBuffer},
-    {"vkResetCommandBuffer", (PFN_vkVoidFunction)(PFN_vkResetCommandBuffer)vkResetCommandBuffer},
-    {"vkResetCommandPool", (PFN_vkVoidFunction)(PFN_vkResetCommandPool)vkResetCommandPool},
-    {"vkCmdBeginRenderPass", (PFN_vkVoidFunction)(PFN_vkCmdBeginRenderPass)vkCmdBeginRenderPass},
-    {"vkCmdEndRenderPass", (PFN_vkVoidFunction)(PFN_vkCmdEndRenderPass)vkCmdEndRenderPass},    
-    {"vkCmdBindVertexBuffers", (PFN_vkVoidFunction)(PFN_vkCmdBindVertexBuffers)vkCmdBindVertexBuffers},
-    {"vkCmdBindIndexBuffer", (PFN_vkVoidFunction)(PFN_vkCmdBindIndexBuffer)vkCmdBindIndexBuffer},    
-    {"vkCmdSetViewport", (PFN_vkVoidFunction)(PFN_vkCmdSetViewport)vkCmdSetViewport},
-    {"vkCmdSetScissor", (PFN_vkVoidFunction)(PFN_vkCmdSetScissor)vkCmdSetScissor},
-    {"vkCmdDraw", (PFN_vkVoidFunction)(PFN_vkCmdDraw)vkCmdDraw},
-    {"vkCmdDrawIndexed", (PFN_vkVoidFunction)(PFN_vkCmdDrawIndexed)vkCmdDrawIndexed},
-    {"vkCmdDrawIndirect", (PFN_vkVoidFunction)(PFN_vkCmdDrawIndirect)vkCmdDrawIndirect},
-    {"vkCmdDrawIndexedIndirect", (PFN_vkVoidFunction)(PFN_vkCmdDrawIndexedIndirect)vkCmdDrawIndexedIndirect},
-    {"vkCmdBindPipeline", (PFN_vkVoidFunction)(PFN_vkCmdBindPipeline)vkCmdBindPipeline},
-    {"vkCmdBindDescriptorSets", (PFN_vkVoidFunction)(PFN_vkCmdBindDescriptorSets)vkCmdBindDescriptorSets},
-    {"vkQueueSubmit", (PFN_vkVoidFunction)(PFN_vkQueueSubmit)vkQueueSubmit},
+    {"vkCreateCommandPool", (PFN_vkVoidFunction)vkCreateCommandPool},
+    {"vkDestroyCommandPool", (PFN_vkVoidFunction)vkDestroyCommandPool},
+    {"vkAllocateCommandBuffers", (PFN_vkVoidFunction)vkAllocateCommandBuffers},
+    {"vkFreeCommandBuffers", (PFN_vkVoidFunction)vkFreeCommandBuffers},
+    {"vkBeginCommandBuffer", (PFN_vkVoidFunction)vkBeginCommandBuffer},
+    {"vkEndCommandBuffer", (PFN_vkVoidFunction)vkEndCommandBuffer},
+    {"vkResetCommandBuffer", (PFN_vkVoidFunction)vkResetCommandBuffer},
+    {"vkResetCommandPool", (PFN_vkVoidFunction)vkResetCommandPool},
+    {"vkCmdBeginRenderPass", (PFN_vkVoidFunction)vkCmdBeginRenderPass},
+    {"vkCmdEndRenderPass", (PFN_vkVoidFunction)vkCmdEndRenderPass},
+    {"vkCmdBindVertexBuffers", (PFN_vkVoidFunction)vkCmdBindVertexBuffers},
+    {"vkCmdBindIndexBuffer", (PFN_vkVoidFunction)vkCmdBindIndexBuffer},
+    {"vkCmdSetViewport", (PFN_vkVoidFunction)vkCmdSetViewport},
+    {"vkCmdSetScissor", (PFN_vkVoidFunction)vkCmdSetScissor},
+    {"vkCmdDraw", (PFN_vkVoidFunction)vkCmdDraw},
+    {"vkCmdDrawIndexed", (PFN_vkVoidFunction)vkCmdDrawIndexed},
+    {"vkCmdDrawIndirect", (PFN_vkVoidFunction)vkCmdDrawIndirect},
+    {"vkCmdDrawIndexedIndirect", (PFN_vkVoidFunction)vkCmdDrawIndexedIndirect},
+    {"vkCmdBindPipeline", (PFN_vkVoidFunction)vkCmdBindPipeline},
+    {"vkCmdBindDescriptorSets", (PFN_vkVoidFunction)vkCmdBindDescriptorSets},
+    {"vkQueueSubmit", (PFN_vkVoidFunction)vkQueueSubmit},
 
     // [shader.cpp]
-    {"vkCreateShaderModule", (PFN_vkVoidFunction)(PFN_vkCreateShaderModule)vkCreateShaderModule},
-    {"vkDestroyShaderModule", (PFN_vkVoidFunction)(PFN_vkDestroyShaderModule)vkDestroyShaderModule},
-    {"vkCreateDescriptorSetLayout", (PFN_vkVoidFunction)(PFN_vkCreateDescriptorSetLayout)vkCreateDescriptorSetLayout},
-    {"vkDestroyDescriptorSetLayout", (PFN_vkVoidFunction)(PFN_vkDestroyDescriptorSetLayout)vkDestroyDescriptorSetLayout},
-    {"vkCreateDescriptorPool", (PFN_vkVoidFunction)(PFN_vkCreateDescriptorPool)vkCreateDescriptorPool},
-    {"vkDestroyDescriptorPool", (PFN_vkVoidFunction)(PFN_vkDestroyDescriptorPool)vkDestroyDescriptorPool},
-    {"vkAllocateDescriptorSets", (PFN_vkVoidFunction)(PFN_vkAllocateDescriptorSets)vkAllocateDescriptorSets},
-    {"vkFreeDescriptorSets", (PFN_vkVoidFunction)(PFN_vkFreeDescriptorSets)vkFreeDescriptorSets},
-    {"vkUpdateDescriptorSets", (PFN_vkVoidFunction)(PFN_vkUpdateDescriptorSets)vkUpdateDescriptorSets},
-    
+    {"vkCreateShaderModule", (PFN_vkVoidFunction)vkCreateShaderModule},
+    {"vkDestroyShaderModule", (PFN_vkVoidFunction)vkDestroyShaderModule},
+    {"vkCreateDescriptorSetLayout", (PFN_vkVoidFunction)vkCreateDescriptorSetLayout},
+    {"vkDestroyDescriptorSetLayout", (PFN_vkVoidFunction)vkDestroyDescriptorSetLayout},
+    {"vkCreateDescriptorPool", (PFN_vkVoidFunction)vkCreateDescriptorPool},
+    {"vkDestroyDescriptorPool", (PFN_vkVoidFunction)vkDestroyDescriptorPool},
+    {"vkAllocateDescriptorSets", (PFN_vkVoidFunction)vkAllocateDescriptorSets},
+    {"vkFreeDescriptorSets", (PFN_vkVoidFunction)vkFreeDescriptorSets},
+    {"vkUpdateDescriptorSets", (PFN_vkVoidFunction)vkUpdateDescriptorSets},
+
     // [screen.cpp]
-    {"vkCreateSwapchainKHR", (PFN_vkVoidFunction)vkCreateSwapchainKHR}, 
+    {"vkCreateSwapchainKHR", (PFN_vkVoidFunction)vkCreateSwapchainKHR},
     {"vkDestroySwapchainKHR", (PFN_vkVoidFunction)vkDestroySwapchainKHR},
-    {"vkGetSwapchainImagesKHR", (PFN_vkVoidFunction)vkGetSwapchainImagesKHR}, 
+    {"vkGetSwapchainImagesKHR", (PFN_vkVoidFunction)vkGetSwapchainImagesKHR},
     {"vkAcquireNextImageKHR", (PFN_vkVoidFunction)vkAcquireNextImageKHR},
     {"vkQueuePresentKHR", (PFN_vkVoidFunction)vkQueuePresentKHR},
-    
-    // [vulkan_layer.cpp]
-    {"vkCreateInstance", (PFN_vkCreateInstance)vkCreateInstance}, 
-    {"vkCreateDevice", (PFN_vkCreateDevice)vkCreateDevice},
-    {"vkEnumeratePhysicalDevices", (PFN_vkEnumeratePhysicalDevices)vkEnumeratePhysicalDevices},
-    {"vkGetPhysicalDeviceProperties", (PFN_vkGetPhysicalDeviceProperties)vkGetPhysicalDeviceProperties},
-    {"vkGetPhysicalDeviceMemoryProperties", (PFN_vkGetPhysicalDeviceMemoryProperties)vkGetPhysicalDeviceMemoryProperties},
 
     // [context.cpp]
     {"vkCreateInstance", (PFN_vkVoidFunction)vkCreateInstance},
     {"vkCreateDevice", (PFN_vkVoidFunction)vkCreateDevice},
     {"vkEnumeratePhysicalDevices", (PFN_vkVoidFunction)vkEnumeratePhysicalDevices},
     {"vkGetPhysicalDeviceProperties", (PFN_vkVoidFunction)vkGetPhysicalDeviceProperties},
-    {"vkGetPhysicalDeviceMemoryProperties", (PFN_vkVoidFunction)vkGetPhysicalDeviceMemoryProperties}, 
-    {"vkCreateFramebuffer", (PFN_vkVoidFunction)(PFN_vkCreateFramebuffer)vkCreateFramebuffer},
-    {"vkDestroyFramebuffer", (PFN_vkVoidFunction)(PFN_vkDestroyFramebuffer)vkDestroyFramebuffer},
+    {"vkGetPhysicalDeviceMemoryProperties", (PFN_vkVoidFunction)vkGetPhysicalDeviceMemoryProperties},
+    {"vkCreateFramebuffer", (PFN_vkVoidFunction)vkCreateFramebuffer},
+    {"vkDestroyFramebuffer", (PFN_vkVoidFunction)vkDestroyFramebuffer},
 };
 
-extern "C" PFN_vkVoidFunction get_vulkan_proc_addr(const char* pName) {
-    for (auto& entry : dispatch_table) if (strcmp(pName, entry.name) == 0) return entry.func;
+extern "C"
+PFN_vkVoidFunction get_vulkan_proc_addr(const char* pName) {
+
+    if (!pName)
+        return nullptr;
+
+    for (const auto& entry : dispatch_table) {
+
+        if (strcmp(entry.name, pName) == 0)
+            return entry.func;
+    }
+
     return nullptr;
 }
