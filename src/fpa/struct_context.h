945053be90385126ad2d1d3826ea20f3f3f9f0a5
The Earth 🌏 <winterwolfvn123@gmail.com>
2026-06-20 14:19:24 +0700

    Create struct_context.h

---
+++ src/fpa/struct_context.h
@@ -0,0 +1,46 @@
+#pragma once
+#include <EGL/egl.h>
+#include <GLES3/gl3.h>
+
+// [context.cpp] 
+struct VkInstance_T {
+    EGLDisplay display;
+};
+typedef VkInstance_T* VkInstance;
+
+struct VkPhysicalDevice_T {
+    const char* gpu_name;
+};
+typedef VkPhysicalDevice_T* VkPhysicalDevice;
+
+struct VkDevice_T {
+    EGLContext context;
+    EGLSurface surface;
+};
+typedef VkDevice_T* VkDevice;
+
+struct VkMemoryType {
+    int32_t propertyFlags; 
+    int32_t heapIndex;
+};
+
+struct VkMemoryHeap {
+    int64_t size;
+    int32_t flags;
+};
+
+struct VkPhysicalDeviceMemoryProperties {
+    int32_t memoryTypeCount;
+    VkMemoryType memoryTypes[4];
+    int32_t memoryHeapCount;
+    VkMemoryHeap memoryHeaps[2];
+};
+
+struct VkPhysicalDeviceProperties {
+    int32_t apiVersion;
+    int32_t driverVersion;
+    int32_t vendorID;
+    int32_t deviceID;
+    char deviceName[256];
+};
+
