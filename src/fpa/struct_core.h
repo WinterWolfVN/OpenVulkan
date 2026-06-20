f41b76dd80f4b279de243d0842fbb771271fc7c5
The Earth 🌏 <winterwolfvn123@gmail.com>
2026-06-20 20:58:29 +0700

    Update struct_core.h

--- src/fpa/struct_core.h
+++ src/fpa/struct_core.h
@@ -2,6 +2,7 @@
 #include <atomic>
 #include <cstdint>
 
+//[buffer.cpp] 
 struct VkDeviceMemory_T {
     void* hostPtr;
     int64_t size;
@@ -16,4 +17,59 @@ struct VkBuffer_T {
     int32_t buffer;    
     int32_t usage;
     struct VkDeviceMemory_T* memory;    
+};
+
+//[draw.cpp] 
+struct VkCommandBuffer_T {
+    int32_t currentTopology;
+    int32_t currentIndexType;
+    int64_t currentIndexOffset;
+};
+typedef struct VkCommandBuffer_T* VkCommandBuffer;
+
+struct VkPipeline_T {
+    int32_t program;
+    int32_t topology;
+};
+typedef struct VkPipeline_T* VkPipeline;
+
+typedef struct VkQueue_T* VkQueue;
+typedef struct VkPipelineLayout_T* VkPipelineLayout;
+typedef struct VkDescriptorSet_T* VkDescriptorSet;
+typedef struct VkFence_T* VkFence;
+
+struct VkViewport {
+    float x;
+    float y;
+    float width;
+    float height;
+    float minDepth;
+    float maxDepth;
+};
+
+struct VkExtent2D {
+    int32_t width;
+    int32_t height;
+};
+
+struct VkOffset2D {
+    int32_t x;
+    int32_t y;
+};
+
+struct VkRect2D {
+    VkOffset2D offset;
+    VkExtent2D extent;
+};
+
+struct VkSubmitInfo {
+    VkStructureType sType;
+    const void* pNext;
+    int32_t waitSemaphoreCount;
+    const void* pWaitSemaphores;
+    const int32_t* pWaitDstStageMask;
+    int32_t commandBufferCount;
+    const VkCommandBuffer* pCommandBuffers;
+    int32_t signalSemaphoreCount;
+    const void* pSignalSemaphores;
 };
