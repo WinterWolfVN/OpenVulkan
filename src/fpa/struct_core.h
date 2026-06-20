cbd7dc66e12f87aec9abf44449538e18d988a062
The Earth 🌏 <winterwolfvn123@gmail.com>
2026-06-20 21:08:02 +0700

    Refactor VkBuffer_T and add new structures

--- src/fpa/struct_core.h
+++ src/fpa/struct_core.h
@@ -12,11 +12,11 @@ struct VkDeviceMemory_T {
 };
 
 struct VkBuffer_T {
+    int32_t buffer;
     int64_t size;
-    int64_t memoryOffset;
-    int32_t buffer;    
     int32_t usage;
-    struct VkDeviceMemory_T* memory;    
+    struct VkDeviceMemory_T* memory;
+    int64_t memoryOffset;
 };
 
 //[draw.cpp] 
@@ -30,12 +30,28 @@ typedef struct VkCommandBuffer_T* VkCommandBuffer;
 struct VkPipeline_T {
     int32_t program;
     int32_t topology;
+    int32_t depthTestEnable;
+    int32_t depthCompareOp;
+    int32_t cullModeEnable;
+    int32_t cullFace;
+    int32_t frontFace;
+    int32_t blendEnable;
+    int32_t blendSrcFactor;
+    int32_t blendDstFactor;
+    int32_t blendEquation;
 };
 typedef struct VkPipeline_T* VkPipeline;
 
+struct VkDescriptorSet_T {
+    int32_t uniformBuffer;
+    int64_t offset;
+    int64_t size;
+    int32_t binding;
+};
+typedef struct VkDescriptorSet_T* VkDescriptorSet;
+
 typedef struct VkQueue_T* VkQueue;
 typedef struct VkPipelineLayout_T* VkPipelineLayout;
-typedef struct VkDescriptorSet_T* VkDescriptorSet;
 typedef struct VkFence_T* VkFence;
 
 struct VkViewport {
@@ -62,8 +78,15 @@ struct VkRect2D {
     VkExtent2D extent;
 };
 
+struct VkCommandBufferBeginInfo {
+    int32_t sType;
+    const void* pNext;
+    int32_t flags;
+    const void* pInheritanceInfo;
+};
+
 struct VkSubmitInfo {
-    VkStructureType sType;
+    int32_t sType;
     const void* pNext;
     int32_t waitSemaphoreCount;
     const void* pWaitSemaphores;
