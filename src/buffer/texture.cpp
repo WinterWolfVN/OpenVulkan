fb29e3ae76ac73f2813b088e02e656b918eda347
The Earth 🌏 <winterwolfvn123@gmail.com>
2026-07-07 06:12:26 +0700

    Update texture.cpp

--- src/buffer/texture.cpp
+++ src/buffer/texture.cpp
@@ -15,6 +15,13 @@ int32_t vkCreateImage(VkDevice device, const VkImageCreateInfo* pCreateInfo, con
     img->depth = pCreateInfo->extent.depth;
     img->mipLevels = pCreateInfo->mipLevels;
     img->arrayLayers = pCreateInfo->arrayLayers;
+    view->image = pCreateInfo->image;
+    view->viewType = pCreateInfo->viewType;
+    view->format = pCreateInfo->format;
+    view->baseMipLevel = pCreateInfo->subresourceRange.baseMipLevel;
+    view->levelCount = pCreateInfo->subresourceRange.levelCount;
+    view->baseArrayLayer = pCreateInfo->subresourceRange.baseArrayLayer;
+    view->layerCount = pCreateInfo->subresourceRange.layerCount;
 
     GLuint gl_tex = 0;
     glGenTextures(1, &gl_tex);
@@ -35,14 +42,18 @@ int32_t vkCreateImage(VkDevice device, const VkImageCreateInfo* pCreateInfo, con
     glBindTexture(target, static_cast<GLuint>(img->texture));
 
     GLenum internalFormat = GL_RGBA8; 
+    int32_t vkFormat = pCreateInfo->format;
     
-    if (target == GL_TEXTURE_2D) {
-        glTexStorage2D(target, img->mipLevels, internalFormat, img->width, img->height);
-    } else if (target == GL_TEXTURE_3D) {
-        glTexStorage3D(target, img->mipLevels, internalFormat, img->width, img->height, img->depth);
-    } else if (target == GL_TEXTURE_2D_ARRAY) {
-        glTexStorage3D(target, img->mipLevels, internalFormat, img->width, img->height, img->arrayLayers);
-    }
+    if (vkFormat == 9) internalFormat = GL_R8;             
+    else if (vkFormat == 16) internalFormat = GL_RG8;      
+    else if (vkFormat == 23) internalFormat = GL_RGB8;     
+    else if (vkFormat == 37) internalFormat = GL_RGBA8;    
+    else if (vkFormat == 43) internalFormat = GL_SRGB8_ALPHA8; 
+    else if (vkFormat == 44) internalFormat = GL_BGRA8_EXT;    
+    else if (vkFormat == 50) internalFormat = GL_SRGB8_ALPHA8; 
+    else if (vkFormat == 124) internalFormat = GL_DEPTH_COMPONENT16; 
+    else if (vkFormat == 126) internalFormat = GL_DEPTH_COMPONENT32F;
+    else if (vkFormat == 129) internalFormat = GL_DEPTH24_STENCIL8;   
 
     glBindTexture(target, 0);
 
@@ -111,6 +122,31 @@ int32_t vkCreateSampler(VkDevice device, const VkSamplerCreateInfo* pCreateInfo,
     glSamplerParameteri(static_cast<GLuint>(sampler->sampler), GL_TEXTURE_WRAP_R, static_cast<GLint>(wrapW));
 
     *pSampler = sampler;
+    glSamplerParameterf(static_cast<GLuint>(sampler->sampler), GL_TEXTURE_MIN_LOD, pCreateInfo->minLod);
+    glSamplerParameterf(static_cast<GLuint>(sampler->sampler), GL_TEXTURE_MAX_LOD, pCreateInfo->maxLod);
+
+    if (pCreateInfo->anisotropyEnable != 0) {        
+        #ifndef GL_TEXTURE_MAX_ANISOTROPY_EXT
+        #define GL_TEXTURE_MAX_ANISOTROPY_EXT 0x84FE
+        #endif
+        glSamplerParameterf(static_cast<GLuint>(sampler->sampler), GL_TEXTURE_MAX_ANISOTROPY_EXT, pCreateInfo->maxAnisotropy);
+    }
+
+    if (pCreateInfo->compareEnable != 0) {
+        glSamplerParameteri(static_cast<GLuint>(sampler->sampler), GL_TEXTURE_COMPARE_MODE, GL_COMPARE_REF_TO_TEXTURE);
+        
+        GLenum cmpOp = GL_LESS;
+        if (pCreateInfo->compareOp == 0) cmpOp = GL_NEVER;
+        else if (pCreateInfo->compareOp == 1) cmpOp = GL_LESS;
+        else if (pCreateInfo->compareOp == 2) cmpOp = GL_EQUAL;
+        else if (pCreateInfo->compareOp == 3) cmpOp = GL_LEQUAL;
+        else if (pCreateInfo->compareOp == 4) cmpOp = GL_GREATER;
+        else if (pCreateInfo->compareOp == 5) cmpOp = GL_NOTEQUAL;
+        else if (pCreateInfo->compareOp == 6) cmpOp = GL_GEQUAL;
+        else if (pCreateInfo->compareOp == 7) cmpOp = GL_ALWAYS;
+        
+        glSamplerParameteri(static_cast<GLuint>(sampler->sampler), GL_TEXTURE_COMPARE_FUNC, static_cast<GLint>(cmpOp));
+}
     return 0;
 }
 
