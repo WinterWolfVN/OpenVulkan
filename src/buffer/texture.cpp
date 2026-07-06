806c010dd9f451b4cc0600eb576f328d87c1b288
The Earth 🌏 <winterwolfvn123@gmail.com>
2026-07-07 06:30:59 +0700

    Update texture.cpp

--- src/buffer/texture.cpp
+++ src/buffer/texture.cpp
@@ -1,11 +1,13 @@
 #include "../struct/stbuffer.h"
 #include <GLES3/gl31.h>
 #include <cstdint>
+#include <cstring>
 
 extern "C" {
 
 int32_t vkCreateImage(VkDevice device, const VkImageCreateInfo* pCreateInfo, const void* pAllocator, VkImage* pImage) {
     if (!device || !pCreateInfo || !pImage) return -3;
+
     VkImage img = new VkImage_T();
     img->texture = 0;
     img->imageType = pCreateInfo->imageType;
@@ -15,13 +17,6 @@ int32_t vkCreateImage(VkDevice device, const VkImageCreateInfo* pCreateInfo, con
     img->depth = pCreateInfo->extent.depth;
     img->mipLevels = pCreateInfo->mipLevels;
     img->arrayLayers = pCreateInfo->arrayLayers;
-    view->image = pCreateInfo->image;
-    view->viewType = pCreateInfo->viewType;
-    view->format = pCreateInfo->format;
-    view->baseMipLevel = pCreateInfo->subresourceRange.baseMipLevel;
-    view->levelCount = pCreateInfo->subresourceRange.levelCount;
-    view->baseArrayLayer = pCreateInfo->subresourceRange.baseArrayLayer;
-    view->layerCount = pCreateInfo->subresourceRange.layerCount;
 
     GLuint gl_tex = 0;
     glGenTextures(1, &gl_tex);
@@ -42,18 +37,27 @@ int32_t vkCreateImage(VkDevice device, const VkImageCreateInfo* pCreateInfo, con
     glBindTexture(target, static_cast<GLuint>(img->texture));
 
     GLenum internalFormat = GL_RGBA8; 
-    int32_t vkFormat = pCreateInfo->format;
-    
-    if (vkFormat == 9) internalFormat = GL_R8;             
-    else if (vkFormat == 16) internalFormat = GL_RG8;      
-    else if (vkFormat == 23) internalFormat = GL_RGB8;     
-    else if (vkFormat == 37) internalFormat = GL_RGBA8;    
-    else if (vkFormat == 43) internalFormat = GL_SRGB8_ALPHA8; 
-    else if (vkFormat == 44) internalFormat = GL_BGRA8_EXT;    
-    else if (vkFormat == 50) internalFormat = GL_SRGB8_ALPHA8; 
-    else if (vkFormat == 124) internalFormat = GL_DEPTH_COMPONENT16; 
-    else if (vkFormat == 126) internalFormat = GL_DEPTH_COMPONENT32F;
-    else if (vkFormat == 129) internalFormat = GL_DEPTH24_STENCIL8;   
+    switch (pCreateInfo->format) {
+        case 9:   internalFormat = GL_R8; break;
+        case 16:  internalFormat = GL_RG8; break;
+        case 23:  internalFormat = GL_RGB8; break;
+        case 37:  internalFormat = GL_RGBA8; break;
+        case 43:  internalFormat = GL_SRGB8_ALPHA8; break;
+        case 44:  internalFormat = GL_RGBA8; break;
+        case 50:  internalFormat = GL_SRGB8_ALPHA8; break;
+        case 124: internalFormat = GL_DEPTH_COMPONENT16; break;
+        case 126: internalFormat = GL_DEPTH_COMPONENT32F; break;
+        case 129: internalFormat = GL_DEPTH24_STENCIL8; break;
+        default:  internalFormat = GL_RGBA8; break;
+    }
+
+    if (target == GL_TEXTURE_2D) {
+        glTexStorage2D(target, img->mipLevels, internalFormat, img->width, img->height);
+    } else if (target == GL_TEXTURE_3D) {
+        glTexStorage3D(target, img->mipLevels, internalFormat, img->width, img->height, img->depth);
+    } else if (target == GL_TEXTURE_2D_ARRAY) {
+        glTexStorage3D(target, img->mipLevels, internalFormat, img->width, img->height, img->arrayLayers);
+    }
 
     glBindTexture(target, 0);
 
@@ -73,10 +77,15 @@ void vkDestroyImage(VkDevice device, VkImage image, const void* pAllocator) {
 
 int32_t vkCreateImageView(VkDevice device, const VkImageViewCreateInfo* pCreateInfo, const void* pAllocator, VkImageView* pView) {
     if (!device || !pCreateInfo || !pView) return -3;
+
     VkImageView view = new VkImageView_T();
     view->image = pCreateInfo->image;
     view->viewType = pCreateInfo->viewType;
     view->format = pCreateInfo->format;
+    view->baseMipLevel = pCreateInfo->subresourceRange.baseMipLevel;
+    view->levelCount = pCreateInfo->subresourceRange.levelCount;
+    view->baseArrayLayer = pCreateInfo->subresourceRange.baseArrayLayer;
+    view->layerCount = pCreateInfo->subresourceRange.layerCount;
 
     *pView = view;
     return 0;
@@ -90,6 +99,7 @@ void vkDestroyImageView(VkDevice device, VkImageView imageView, const void* pAll
 
 int32_t vkCreateSampler(VkDevice device, const VkSamplerCreateInfo* pCreateInfo, const void* pAllocator, VkSampler* pSampler) {
     if (!device || !pCreateInfo || !pSampler) return -3;
+
     VkSampler sampler = new VkSampler_T();
     sampler->sampler = 0;
 
@@ -121,32 +131,38 @@ int32_t vkCreateSampler(VkDevice device, const VkSamplerCreateInfo* pCreateInfo,
     glSamplerParameteri(static_cast<GLuint>(sampler->sampler), GL_TEXTURE_WRAP_T, static_cast<GLint>(wrapV));
     glSamplerParameteri(static_cast<GLuint>(sampler->sampler), GL_TEXTURE_WRAP_R, static_cast<GLint>(wrapW));
 
-    *pSampler = sampler;
     glSamplerParameterf(static_cast<GLuint>(sampler->sampler), GL_TEXTURE_MIN_LOD, pCreateInfo->minLod);
     glSamplerParameterf(static_cast<GLuint>(sampler->sampler), GL_TEXTURE_MAX_LOD, pCreateInfo->maxLod);
 
-    if (pCreateInfo->anisotropyEnable != 0) {        
-        #ifndef GL_TEXTURE_MAX_ANISOTROPY_EXT
-        #define GL_TEXTURE_MAX_ANISOTROPY_EXT 0x84FE
-        #endif
-        glSamplerParameterf(static_cast<GLuint>(sampler->sampler), GL_TEXTURE_MAX_ANISOTROPY_EXT, pCreateInfo->maxAnisotropy);
+    if (pCreateInfo->anisotropyEnable != 0) {
+        const char* ext = reinterpret_cast<const char*>(glGetString(GL_EXTENSIONS));
+        if (ext && std::strstr(ext, "GL_EXT_texture_filter_anisotropic")) {
+            #ifndef GL_TEXTURE_MAX_ANISOTROPY_EXT
+            #define GL_TEXTURE_MAX_ANISOTROPY_EXT 0x84FE
+            #endif
+            glSamplerParameterf(static_cast<GLuint>(sampler->sampler), GL_TEXTURE_MAX_ANISOTROPY_EXT, pCreateInfo->maxAnisotropy);
+        }
     }
 
     if (pCreateInfo->compareEnable != 0) {
         glSamplerParameteri(static_cast<GLuint>(sampler->sampler), GL_TEXTURE_COMPARE_MODE, GL_COMPARE_REF_TO_TEXTURE);
         
         GLenum cmpOp = GL_LESS;
-        if (pCreateInfo->compareOp == 0) cmpOp = GL_NEVER;
-        else if (pCreateInfo->compareOp == 1) cmpOp = GL_LESS;
-        else if (pCreateInfo->compareOp == 2) cmpOp = GL_EQUAL;
-        else if (pCreateInfo->compareOp == 3) cmpOp = GL_LEQUAL;
-        else if (pCreateInfo->compareOp == 4) cmpOp = GL_GREATER;
-        else if (pCreateInfo->compareOp == 5) cmpOp = GL_NOTEQUAL;
-        else if (pCreateInfo->compareOp == 6) cmpOp = GL_GEQUAL;
-        else if (pCreateInfo->compareOp == 7) cmpOp = GL_ALWAYS;
-        
+        switch (pCreateInfo->compareOp) {
+            case 0: cmpOp = GL_NEVER; break;
+            case 1: cmpOp = GL_LESS; break;
+            case 2: cmpOp = GL_EQUAL; break;
+            case 3: cmpOp = GL_LEQUAL; break;
+            case 4: cmpOp = GL_GREATER; break;
+            case 5: cmpOp = GL_NOTEQUAL; break;
+            case 6: cmpOp = GL_GEQUAL; break;
+            case 7: cmpOp = GL_ALWAYS; break;
+            default: cmpOp = GL_LESS; break;
+        }
         glSamplerParameteri(static_cast<GLuint>(sampler->sampler), GL_TEXTURE_COMPARE_FUNC, static_cast<GLint>(cmpOp));
-}
+    }
+
+    *pSampler = sampler;
     return 0;
 }
 
