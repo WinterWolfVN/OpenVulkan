da3c4ca7f8e70ba34207874fc14405e3ffcb40dc
The Earth 🌏 <winterwolfvn123@gmail.com>
2026-07-12 06:00:25 +0700

    Update draw.cpp

--- src/command/draw.cpp
+++ src/command/draw.cpp
@@ -97,7 +97,7 @@ void vkCmdSetViewport(VkCommandBuffer commandBuffer, int32_t firstViewport, int3
     if (!commandBuffer || !pViewports || viewportCount <= 0) return;    
     VkViewport vp = pViewports[0];    
     commandBuffer->commands.push_back([vp]() {
-        // Bug        
+        int32_t renderTargetHeight = commandBuffer->renderTargetHeight;     
         glViewport(static_cast<GLint>(std::floor(vp.x)),
                    static_cast<GLint>(std::floor(renderTargetHeight - (vp.y + vp.height))),
                    static_cast<GLsizei>(std::floor(vp.width)),
@@ -110,7 +110,7 @@ void vkCmdSetScissor(VkCommandBuffer commandBuffer, int32_t firstScissor, int32_
     if (!commandBuffer || !pScissors || scissorCount <= 0) return;    
     VkRect2D sc = pScissors[0];    
     commandBuffer->commands.push_back([sc]() {
-        // Bug     
+        int32_t renderTargetHeight = commandBuffer->renderTargetHeight;     
         glScissor(static_cast<GLint>(sc.offset.x),
                   static_cast<GLint>(renderTargetHeight - (sc.offset.y + sc.extent.height)),
                   static_cast<GLsizei>(sc.extent.width),
