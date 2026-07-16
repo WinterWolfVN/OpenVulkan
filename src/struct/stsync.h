0b723a880a6edb6c4085294fb37c19a2887675f9
The Earth 🌏 <winterwolfvn123@gmail.com>
2026-07-16 09:03:54 +0700

    Update stsync.h

--- src/struct/stsync.h
+++ src/struct/stsync.h
@@ -33,3 +33,43 @@ struct VkEvent_T {
     int32_t flags;
 };
 typedef struct VkEvent_T* VkEvent;
+
+struct VkSemaphoreTypeCreateInfo {
+    const void* pNext;
+    int64_t initialValue;    
+    int32_t sType;
+    int32_t semaphoreType;
+};
+
+struct VkSemaphoreCreateInfo {
+    const void* pNext;    
+    int32_t sType;
+    int32_t flags;
+};
+
+struct VkSemaphoreSignalInfo {
+    const void* pNext;
+    VkSemaphore semaphore;
+    uint64_t value;    
+    int32_t sType;
+};
+
+struct VkSemaphoreWaitInfo {
+    const void* pNext;
+    const VkSemaphore* pSemaphores;
+    const uint64_t* pValues;    
+    int32_t sType;
+    int32_t flags;
+    int32_t semaphoreCount;
+};
+
+struct VkSemaphore_T {
+    int64_t semaphoreId;
+    uint64_t currentValue;
+    uint64_t pendingValue;
+    GLsync pendingSync;   
+    int32_t type;
+    int32_t signaled;
+    int32_t flags;
+};
+typedef struct VkSemaphore_T* VkSemaphore;
